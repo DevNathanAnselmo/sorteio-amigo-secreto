2995b8581b5d5a31fffea4848409442a6bf9df26
NathanAnselmo <nathan-vanzzo@hotmail.com>
2025-01-30 23:18:28 -0300

    adiconado estrutura condicional para evitar nome em branco

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -4,6 +4,10 @@ function adicionarAmigo(){
     limparCampo();
     listaDeNomes.push(inserirNomes);
     console.log(listaDeNomes);
+    if (listaDeNomes.includes('')){
+        listaDeNomes.pop();
+        alert('Você não pode adicionar um nome vazio!');
+    }
 }
 function limparCampo(){
     inserirNomes = document.querySelector('input').value = '';

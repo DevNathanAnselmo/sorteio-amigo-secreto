757aff92debc9fd2ffc58fa3e04365a2d5419f17
NathanAnselmo <nathan-vanzzo@hotmail.com>
2025-01-31 09:40:46 -0300

    criado o sorteio do nome

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,17 +1,24 @@
 listaDeNomes = [];
-function adicionarAmigo(){
+nomeEscolhido = sortearAmigo();
+function adicionarAmigo(){              // função que adiciona um nome a lista de nomes a serem sorteados.
     let inserirNomes = document.getElementById('amigo').value;
-    if (inserirNomes == ''){
-        alert('Você não pode adicionar um nome vazio!');
+    if (inserirNomes == '' || listaDeNomes.includes(inserirNomes)){
+        inserirNomes == '' ? alert('Você não pode adicionar um nome vazio!'): alert('esse amigo já está na lista!');
+        limparCampo();
     }else{
         listaDeNomes.push(inserirNomes);
         limparCampo();
         console.log(listaDeNomes);
     }
 }
-function limparCampo(){
+function limparCampo(){                  // limpa o nome do campo de digitação após ser inserido na lista.
     inserirNomes = document.querySelector('input').value = '';
 }
-
+function sortearAmigo(){                 // sorteia um dos nomes que estão na lista.
+    let nomeSorteado = parseInt(Math.random() * listaDeNomes.length);
+    let nome = listaDeNomes[nomeSorteado];
+    console.log(nome);
+    return nome;
+}
 
 

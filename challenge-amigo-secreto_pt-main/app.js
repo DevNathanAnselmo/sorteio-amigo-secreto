02c241523a10f6e1c009924f5e17c08f4142410e
NathanAnselmo <nathan-vanzzo@hotmail.com>
2025-01-30 23:00:49 -0300

    adicionar nomes a lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+listaDeNomes = [];
+function adicionarAmigo(){
+    let inserirNomes = document.getElementById('amigo').value;
+    listaAmigos.innerHTML = listaDeNomes;
+    limparCampo();
+    listaDeNomes.push(inserirNomes);
+    console.log(listaDeNomes);
+}
+function limparCampo(){
+    inserirNomes = document.querySelector('input').value = '';
+}
+
+
+
